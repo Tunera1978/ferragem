fa1ba68eb583a317458c62ba97c9ee1622aed692
Aluno <Aluno@LAB1PC13.ifspcapivari.edu.br>
2015-09-10 22:19:58 -0300

    criando DAO 9 de 14

--- ferragem/src/dao/TipoEndDAO.java
+++ ferragem/src/dao/TipoEndDAO.java
@@ -1,100 +1,111 @@
-
 package dao;
 
-
-import beans.Produto;
+import beans.TipoEndereco;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class TipoEndDAO extends GenericDAO {
-    
-    public TipoEndDAO(){
-        super();//a classe mãe pega a conexão
+public class TipoEndDAO extends GenericDAO
+{
+
+  public TipoEndDAO()
+  {
+    super();//a classe mãe pega a conexão
+  }
+
+  public boolean inserir(TipoEndereco tipoEndereco)
+  {
+    String sql = "INSERT INTO tbltipoEndereco( descricao ) VALUES( ? )";
+    try
+    {
+      this.prepareStmte(sql);
+      this.stmte.setString(2, tipoEndereco.getDescricao());
+      this.stmte.execute();
+      return true;
     }
-    
-        public boolean inserir(Produto produto){
-        String sql = "INSERT INTO tblproduto(descricao, pesoBarra) VALUES(?, ?)";
-        try{
-            this.prepareStmte(sql);
-            //this.stmte.setInt(1, produto.getIdProduto());
-            this.stmte.setString(1, produto.getDescricao());
-            this.stmte.setFloat(2, produto.getPeso() );
-            this.stmte.execute();
-            return true;
-        }
-        catch(Exception e){
-            return false;
-        }
+    catch (Exception e)
+    {
+      return false;
     }
-    
-    public Produto getProdutoById(int idproduto){
-        Produto u = new Produto();
-        String sql = "SELECT * FROM tblproduto WHERE idproduto = ?";
-        try{
-            this.prepareStmte(sql);
-            this.stmte.setInt(1, idproduto);//parametro
-            ResultSet rs = this.stmte.executeQuery();//return um resultset
-            rs.first();//ResultSet na primeira posição
-            u.setIdProduto(rs.getInt("idProduto"));
-            u.setDescricao(rs.getString("descricao"));
-            return u;
-        }
-        catch(Exception e){
-            return null;// não tem produto para retornar retorna null
-        }
+  }
+
+  public TipoEndereco getTipoEnderecoById(int idtipoEndereco)
+  {
+    TipoEndereco u = new TipoEndereco();
+    String sql = "SELECT * FROM tbltipoEndereco WHERE idtbltipoendereco = ?";
+    try
+    {
+      this.prepareStmte(sql);
+      this.stmte.setInt(1, idtipoEndereco);//parametro
+      ResultSet rs = this.stmte.executeQuery();//return um resultset
+      rs.first();//ResultSet na primeira posição
+      u.setIdtbltipoendereco(rs.getInt("idtbltipoendereco"));
+      u.setDescricao(rs.getString("descricao"));
+      return u;
     }
-    
-    public boolean excluir(Produto u){
-        String sql = "DELETE FROM tblproduto WHERE idproduto = ?";
-        try{
-            this.prepareStmte(sql);
-            this.stmte.setInt(1, u.getIdProduto());//1 significa o 1º que será visto no caso idproduto
-            this.stmte.execute();
-            return true;
-        }
-        catch(Exception e){
-            return false;
-        }   
+    catch (Exception e)
+    {
+      return null;// não tem tipoEndereco para retornar retorna null
     }
-    
-    public boolean editar(Produto produto){
-        String sql = "UPDATE tblproduto SET descricao = ? WHERE idproduto = ?";
-        try{
-            this.prepareStmte(sql);
-            this.stmte.setInt(2, produto.getIdProduto());
-            this.stmte.setString(1, produto.getDescricao());
-            this.stmte.executeUpdate();
-            return true;
-        }
-        catch(Exception e){
-            return false;
-        }
+  }
+
+  public boolean excluir(TipoEndereco u)
+  {
+    String sql = "DELETE FROM tbltipoEndereco WHERE idtbltipoendereco = ?";
+    try
+    {
+      this.prepareStmte(sql);
+      this.stmte.setInt(1, u.getIdtbltipoendereco());//1 significa o 1º que será visto no caso idtipoEndereco
+      this.stmte.execute();
+      return true;
     }
-    
-    public ArrayList<Produto> getProdutoByLista(String descricao){
-       ArrayList<Produto> produto = new ArrayList<Produto>();
-        String sql = "SELECT * FROM tblproduto WHERE descricao LIKE ?";
-        
-        try
-        {
-            this.prepareStmte(sql);
-            this.stmte.setString(1, "%"+descricao+"%");
-            ResultSet rs = this.stmte.executeQuery(); //sempre usar quando fazer uma consulta(SELECT)
-           
-            while(rs.next())
-            {
-                Produto u = new Produto();
-                u.setIdProduto(rs.getInt("idproduto"));
-                u.setDescricao(rs.getString("descricao"));                
-                produto.add(u);
-            }
-            return produto;
-            
-        }
-        catch(Exception e)
-        {
-            return null;
-        }  
+    catch (Exception e)
+    {
+      return false;
     }
-    
+  }
+
+  public boolean editar(TipoEndereco tipoEndereco)
+  {
+    String sql = "UPDATE tbltipoEndereco SET descricao = ? WHERE idtipoEndereco = ?";
+    try
+    {
+      this.prepareStmte(sql);
+      this.stmte.setInt(2, tipoEndereco.getIdtbltipoendereco());
+      this.stmte.setString(1, tipoEndereco.getDescricao());
+      this.stmte.executeUpdate();
+      return true;
+    }
+    catch (Exception e)
+    {
+      return false;
+    }
+  }
+
+  public ArrayList<TipoEndereco> getTipoEnderecoByLista(String descricao)
+  {
+    ArrayList<TipoEndereco> tipoEndereco = new ArrayList<TipoEndereco>();
+    String sql = "SELECT * FROM tbltipoEndereco WHERE descricao LIKE ?";
+
+    try
+    {
+      this.prepareStmte(sql);
+      this.stmte.setString(1, "%" + descricao + "%");
+      ResultSet rs = this.stmte.executeQuery(); //sempre usar quando fazer uma consulta(SELECT)
+
+      while (rs.next())
+      {
+        TipoEndereco u = new TipoEndereco();
+        u.setIdtbltipoendereco(rs.getInt("idtbltipoendereco"));
+        u.setDescricao(rs.getString("descricao"));
+        tipoEndereco.add(u);
+      }
+      return tipoEndereco;
+
+    }
+    catch (Exception e)
+    {
+      return null;
+    }
+  }
+
 }
